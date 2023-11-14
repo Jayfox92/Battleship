04708f788dbba5e7283304fdaeafe718e5bd84ab
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-14 14:55:54 +0100

    jobbar på server-client kommunikation

--- src/main/java/com/teamd/battleship/Server.java
+++ src/main/java/com/teamd/battleship/Server.java
@@ -1,9 +1,6 @@
 package com.teamd.battleship;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -16,11 +13,14 @@ public class Server {
         try {
             serverSocket = new ServerSocket(8080);
             socket = serverSocket.accept();
-            System.out.println("Success");
+            System.out.println("Successful connection");
             InputStream inputStream = socket.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader reader = new BufferedReader(inputStreamReader);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
             String message = reader.readLine();
+            Battleship serverShip = new Battleship();
+            serverShip.readFirstLetter(message);
         }
         catch (IOException e){
             System.out.println(e.getMessage());
