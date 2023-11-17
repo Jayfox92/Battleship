4bd9fff028c87f447ef586efb0b8fd5572c2c645
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-17 15:49:43 +0100

    kommunikation i Battleship, decidenextaction, checkcoordinates & readcoordinates fixade error när skepp koordinater sparades

--- src/main/java/com/teamd/battleship/ClientMain.java
+++ src/main/java/com/teamd/battleship/ClientMain.java
@@ -1,9 +1,12 @@
 package com.teamd.battleship;
 
+import javafx.application.Application;
+
 public class ClientMain {
     public static void main(String[] args) {
         Client client = new Client();
         client.connect();
-        //Application.launch(HelloApplication.class, args);
+        Application.launch(HelloApplication.class, args);
+
     }
 }
