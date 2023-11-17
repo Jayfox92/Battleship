f87ced177b043492d06bd273fe50a0a22416379a
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-17 14:14:54 +0100

    kommunikation i Battleship, decidenextaction, checkcoordinates & readcoordinates

--- src/main/java/com/teamd/battleship/Ship.java
+++ src/main/java/com/teamd/battleship/Ship.java
@@ -12,11 +12,12 @@ public class Ship {
     private int hits = 0;
     private String name;
     private String description;
-    private List<Integer> coordinatesOfShip = new ArrayList<>();
+    private List<Integer> coordinatesOfShip;
 
     public Ship(int length, String name, String description) {
         this.length = length;
         this.name = name;
+        coordinatesOfShip = new ArrayList<>();
     }
 
     public int getLength() {
@@ -44,7 +45,7 @@ public class Ship {
     }
 
     public void setHits(int hits) {
-        this.hits = hits;
+        this.hits = hits+this.hits;
     }
 
     public String getName() {
@@ -79,7 +80,7 @@ public class Ship {
         this.description = description;
     }
 
-    public void setCoordinatesOfShip(int y, int x){
+    public void setCoordinatesOfShip(int y, int x){ //sparar koordinaterna där skeppet existerar. index 0 = y, index 1 = x. index 2 = y, index 3 = x osv osv
         coordinatesOfShip.add(y);
         coordinatesOfShip.add(x);
     }
