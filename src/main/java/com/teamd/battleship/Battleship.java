696957f147f9f781df19214ca70968b601174ff5
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-17 14:14:54 +0100

    kommunikation i Battleship, decidenextaction, checkcoordinates & readcoordinates

--- src/main/java/com/teamd/battleship/Battleship.java
+++ src/main/java/com/teamd/battleship/Battleship.java
@@ -34,8 +34,7 @@ public class Battleship {
     int mapSizeY;
     String water;
     String ownMessage;
-    String opponentMessage;
-    Ship localShip;
+    static String opponentMessage;
 
 
 
@@ -61,23 +60,19 @@ public class Battleship {
             I{"","","","","","","","","","",},
             J{"","","","","","","","","","",},
     };*/
-    private boolean gameActive = false;
-    public void readFirstLetter(String message) { // detta läser första bokstaven som enl. protokoll kommer kunna identifiera vilken 'action' som görs
+
+
+
+    public void decideNextAction(String message){
         message = message.trim();
         message = message.toLowerCase();
-        decideNextAction(message.charAt(0));
-    }
-
-
-    public void decideNextAction(char action){
-        Random random = new Random();
+        char action = message.charAt(0);
         if (action=='i'){ //init //random
-
-            System.out.println("The client sho at "+"random coordinates");
-
+        readCoordinates(message);
+        //slumpa nytt skott
         }
         else if (action=='h'){//shot hit //ai
-            System.out.println("The ");
+
         }
         else if (action=='m'){//shot miss //random
 
@@ -93,7 +88,7 @@ public class Battleship {
 
 
 
-    public List<Integer> readCoordinates(String string){ // Y-koordinat lagras i index 0, X-koordinat lagras i index 1, returneras som en arraylist
+    public void readCoordinates(String string){ // Y-koordinat lagras i index 0, X-koordinat lagras i index 1, returneras som en arraylist
         List<Integer> listOfShotCoordinates = new ArrayList<>();
         string = string.trim();
         string = string.toLowerCase();
@@ -108,75 +103,41 @@ public class Battleship {
         charsMappedToInt.put('h',7);
         charsMappedToInt.put('i',8);
         charsMappedToInt.put('j',9);
-        char findPosOfY = string.charAt((string.length()-1));
+        int indexOfY = string.length()-1;
+        char findPosOfY = string.charAt(indexOfY);
         int yCoordinate = charsMappedToInt.get(findPosOfY);
         listOfShotCoordinates.add(yCoordinate);
-        char findPosOfX = string.charAt((string.length()-2));
+        int indexOfX = string.length()-2;
+        char findPosOfX = string.charAt(indexOfX);
         int xCooordinate = Character.getNumericValue(findPosOfX);
         listOfShotCoordinates.add(xCooordinate);
         checkCoordinates(listOfShotCoordinates);
-        return listOfShotCoordinates;
 
     }
 
 
 
     public void checkCoordinates(List<Integer> list){
-        if(Objects.equals(water, map[list.get(0)][list.get(1)])){
-            //innehåller koordinaterna vatten = miss
-            ownMessage = "m shot ";
+        if(Objects.equals(water, map[list.get(0)][list.get(1)])){ //innehåller koordinaterna vatten = miss
+
+            ownMessage = "m shot";
             //kör metod för nytt skott
         }
-        else if (Objects.equals("s", map[list.get(0)][list.get(1)])) {
-            //innehåller koordinaterna s som innebär skepp = träff
-            //kontroll om skepp är sänkt
-            // vi har en lista (för varje objekt av Ship.java) som sparar koordinaterna där skeppen existerar
-            //första koordinaten y lagrax i listan på index 0, första koodrinaten x lagras i listan på index 1, andra koordinaten där skeppet 'existerar' lagras y koordinat i index 2, x koordinat i index 3 osv osv osv
-            Ship tempShip;
+        else if (Objects.equals("s", map[list.get(0)][list.get(1)])) { //innehåller koordinaterna s (temp sträng för skepp i skrivande stund) = träff
+            //när skepp placeras i 2d array map sparas även koodrinaterna i en arraylist i Ship.java, så att vi får en koppling mellan några koordinater & ett specifikt skepp
+            for (int i = 0; i < shipList.size(); i++) { //loopar igenom alla skepp
 
-            for (int i = 0; i < shipList.size(); i++) { //loopar igenom listan av alla skepp
-                List listofYCoordinates = shipList.get(i).getCoordinatesOfShip();
-                int tempY = 0;
-                /*if (shipList.get(i).getCoordinatesOfShip().contains(list.get(0)) && shipList.get(i).getCoordinatesOfShip().get(1) == list.get(1)) {
-                    int indexOfYInCoordinateList = shipList.get(i).getCoordinatesOfShip().contains(list.get(0));
-                    for ()
-                }*/
-                //tempShip = shipList.get(i);
-                //}
-                for (int j=0; j < shipList.get(i).getCoordinatesOfShip().size(); j++){ // loopar igenom alla koordinater där skeppet 'existerar'
-                    int removeXCoordinates = 1;
-                    listofYCoordinates.remove(removeXCoordinates);
-                    if (removeXCoordinates+1 == shipList.get(i).getCoordinatesOfShip().size()){ //failsafe, om storleken på Getcoordinatesofship är av size 6, kommer removeXcoordinate nå värdet 5, och for loopen körs igen för storleken på listan
-                    int removeLast = listofYCoordinates.size();  // index 0 1 2
-                    listofYCoordinates.remove((removeLast-1));   // size  1 2 3
-                    break;
-                    }
-                    removeXCoordinates = removeXCoordinates+2;
-                    if (listofYCoordinates.contains(list.get(0)) && shipList.get(i).getCoordinatesOfShip().get(1) == list.get(1))){
-
-                    }
-
+                for (int j=0; j < shipList.get(i).getCoordinatesOfShip().size(); j++) { // loopar igenom alla koordinater där skeppet 'existerar'
+                    try { //try catch med tomt catch-block för att hantera out-of-bounds: (j+1)
+                        if (shipList.get(i).getCoordinatesOfShip().get(j).equals(list.get(0)) && shipList.get(i).getCoordinatesOfShip().get(j+1).equals(list.get(1))) {
+                            shipList.get(i).setHits(1);
+                            if (shipList.get(i).isSunk()) {
+                                ownMessage = "s shot";
+                            } else ownMessage = "h shot";
+                        }
+                    } catch (Exception ignore) {}
                 }
 
-                //alla koordinater där ett skepp existerar lagras i en lista som nås genom Ship.getCordinates()
-                //listan är av typen int
-                //listan vet inte om den lagrar en koordinat som motsvarar Y eller X
-                //det blir en 'dålig' for-loop om den skall gå igenom
-
-                tempY++;
-
-                if (tempShip.isSunk()) {
-                    ownMessage = "s shot";
-                } else ownMessage = "h shot";
-
-
-                Ship exampleShip = new Ship(4, "ExampleShip", "NotReal"); //rad skall bort, får ej skapa nytt objekt
-                boolean temp = exampleShip.isSunk();
-                if (temp)  //if-condition skall kontrollera om objektet (Ship) på koordinaterna blir sänkt eller ej
-                    ownMessage = "s shot";
-                else {
-                    ownMessage = "h shot";
-                }
             }
         }
         else System.out.println("Error comparing coordinates to map");
@@ -299,7 +260,7 @@ public class Battleship {
                             for (int k = shipX; k < (shipX + 1) + (shipList.get(i).getLength()-1); k++) {
                                 // map[j][k] = String.valueOf(shipList.get(i).getLength());
                                 map[j][k] = "s";
-                                int tempY = Integer.valueOf(Arrays.toString(map[j]));
+                                int tempY = Integer.valueOf(Arrays.toString(map[j])); //sparar koordinaterna där skeppet existerar. index 0 = y, index 1 = x. index 2 = y, index 3 = x osv osv
                                 String tempString = map[j][k];
                                 int tempX = Integer.valueOf(tempString);
                                 shipList.get(i).setCoordinatesOfShip(tempY,tempX);
@@ -349,6 +310,10 @@ public class Battleship {
                             for (int k = shipX; k < shipX + 1; k++) {
                                // map[j][k] = String.valueOf(shipList.get(i).getLength());
                                 map[j][k] = "s";
+                                int coordinateY = Integer.valueOf(Arrays.toString(map[j])); //sparar koordinaterna där skeppet existerar i en arraylist. index 0 = y, index 1 = x. index 2 = y, index 3 = x osv osv
+                                String stringOfX = map[j][k];
+                                int coordinateX = Integer.valueOf(stringOfX);
+                                shipList.get(i).setCoordinatesOfShip(coordinateY,coordinateX);
                             }
                         }
                         successfulPlacement = true;
