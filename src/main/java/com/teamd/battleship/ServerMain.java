e63719c58e46f029ac0325223af8fde8578927f4
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-19 17:37:24 +0100

    början att knyta ihop javafx med spel-logiken

--- src/main/java/com/teamd/battleship/ServerMain.java
+++ src/main/java/com/teamd/battleship/ServerMain.java
@@ -1,11 +1,17 @@
 package com.teamd.battleship;
 
+import javafx.application.Application;
+
 public class ServerMain {
 
     public static void main(String[] args) {
         Battleship serverShip = new Battleship();
         Server server = new Server("initial message",serverShip);
         serverShip.setServer(server);
+        HelloApplication javafx = new HelloApplication();
+        serverShip.setHelloApplication(javafx);
+        javafx.setBattleship(serverShip);
+        Application.launch(HelloApplication.class, args);
         server.connect();
     }
 }
---
+++ src/main/java/com/teamd/battleship/TestOfThread.java
@@ -0,0 +1,14 @@
+package com.teamd.battleship;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.Scanner;
+
+public class TestOfThread {
+    public static void main(String[] args) {
+
+        Application.launch(HelloApplication.class, args);
+
+    }
+}
