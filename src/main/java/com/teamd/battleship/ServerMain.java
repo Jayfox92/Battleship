498dce596106d93921e306236f2700ca2b642220
Jayfox92 <145023854+Jayfox92@users.noreply.github.com>
2023-11-20 19:58:28 +0100

    äntligen, inte mycket kvar nu!

--- src/main/java/com/teamd/battleship/ServerMain.java
+++ src/main/java/com/teamd/battleship/ServerMain.java
@@ -7,8 +7,7 @@ public class ServerMain {
 
     public static void main(String[] args) {
         System.setProperty("appMode","server");
-        new Thread(()->{
-            Application.launch(HelloApplication.class, args);}).start();
+        Application.launch(HelloApplication.class, args);
 
     }
 }
